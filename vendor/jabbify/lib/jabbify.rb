54ed9ff2a74d2da028d053d12ba8203840481876
Matt Puchlerz <matt@puchlerz.com>
2009-05-19 20:18:29 -0500

    Made quite a few more more #deliver specs

--- vendor/jabbify/lib/jabbify.rb
+++ vendor/jabbify/lib/jabbify.rb
@@ -5,7 +5,14 @@ class Jabbify
   
   def self.deliver(options)
     [ :api_key, :name, :message ].each do |option|
-      raise ArgumentError, "Missing the :#{ option } option" unless options.include? option
+      raise ArgumentError, "Missing the :#{ option } option" unless options[option]
+    end
+
+    begin
+      RestClient.get 'https://jabbify.com:8443/message_push?key=123456&name=Neo&type=message&action=create&message=sweet'
+      true
+    rescue
+      false
     end
   end
   
--- vendor/jabbify/spec/jabbify_spec.rb
+++ vendor/jabbify/spec/jabbify_spec.rb
@@ -2,18 +2,17 @@ require File.join( File.dirname(__FILE__), 'spec_helper' )
 
 describe Jabbify do
   
+  def defaults(options = {})
+    { :api_key => 'qwer1234qwer1234',
+      :type    => :message,
+      :action  => :create,
+      :name    => 'John Doe',
+      :message => 'This is the message!',
+      :to      => 'Jane Doe'
+    }.merge(options)
+  end
+  
   context "delivering messages" do
-    
-    def defaults_except(option)
-      {
-        :api_key => 'qwer1234qwer1234',
-        :type    => :message,
-        :action  => :create,
-        :name    => 'John Doe',
-        :message => 'This is the message!',
-        :to      => 'Jane Doe'
-      }.reject { |key, val| key == option }
-    end
     
     it do
       Jabbify.should respond_to(:deliver)
@@ -23,21 +22,26 @@ describe Jabbify do
       lambda { Jabbify.deliver }.should raise_error(ArgumentError)
     end
   
-    it "should fail if #deliver isn't passed the :api_key option" do
-      lambda { Jabbify.deliver defaults_except(:api_key) }.should raise_error(ArgumentError)
+    it "should fail if #deliver is passed a nil :api_key option" do
+      lambda { Jabbify.deliver defaults(:api_key => nil) }.should raise_error(ArgumentError)
     end
     
-    it "should fail if #deliver isn't passed the :name option" do
-      lambda { Jabbify.deliver defaults_except(:name) }.should raise_error(ArgumentError)      
+    it "should fail if #deliver is passed a nil :name option" do
+      lambda { Jabbify.deliver defaults(:name => nil) }.should raise_error(ArgumentError)      
     end
   
-    it "should fail if #deliver isn't passed the :message option" do
-      lambda { Jabbify.deliver defaults_except(:message) }.should raise_error(ArgumentError)      
+    it "should fail if #deliver is passed a nil :message option" do
+      lambda { Jabbify.deliver defaults(:message => nil) }.should raise_error(ArgumentError)      
     end
     
-    it "should deliver the message to Jabbify if all required options are passed" do
-      pending
-      Jabbify.deliver defaults_except
+    it "should not deliver to Jabbify if the request fails" do
+      RestClient.should_receive(:get).and_raise(RuntimeError)
+      Jabbify.deliver(defaults).should == false
+    end
+    
+    it "should deliver to Jabbify if the request succeeds" do
+      RestClient.should_receive(:get).and_return('body of response')
+      Jabbify.deliver(defaults).should == true
     end
   
   end
