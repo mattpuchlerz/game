bcabfc2058138c56cbe3251130e6afc0f3af7be6
Matt Puchlerz <matt@puchlerz.com>
2009-05-27 10:08:51 -0500

    Single quotes

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,12 +1,12 @@
-OpenAjax.hub.subscribe("jabbify.*.*", function(name, results){
+OpenAjax.hub.subscribe('jabbify.*.*', function(name, results){
   console.log(name);
   console.log(results.data);
 });
 
 window.sendJab = function(){
-  Jabbify.send("message", "create", { message: "hello world" });
+  Jabbify.send('message', 'create', { message: 'hello world' });
 }
 
-Jabbify.connect({ name: "Matt" }, sendJab);
+Jabbify.connect({ name: 'Matt' }, sendJab);
 
 document.getElementById('button').onclick = window.sendJab;
