d2a1d8d1a2beb5e4cb212a7d4e997b1d44633ede
Matt Puchlerz <matt@puchlerz.com>
2009-06-16 14:48:04 -0700

    Made real specs for the player.

--- public/dodgebomb/specs/player_spec.js
+++ public/dodgebomb/specs/player_spec.js
@@ -1,11 +1,20 @@
+var player;
+
 describe('DodgeBomb.Player', {
   
-  'should be true': function() {
-    value_of(true).should_be(true);
+  'before all': function() {
+    
+    // Mock the viewport's element
+    Dodgebomb.Singleton.viewport = {
+      'element': new Element('div').inject(document.body)
+    }
+    
+    player = new Dodgebomb.Player();
   },
   
-  'should be false': function() {
-    value_of(false).should_be(false);
+  'should have a main element': function() {
+    value_of($type(player.element)).should_be('element');
+    value_of(player.element.getParent()).should_be(Dodgebomb.Singleton.viewport.element);
   }
-  
+    
 });
