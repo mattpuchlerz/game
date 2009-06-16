51ff1f0ff41fc2c0414cbb28c6f0f892fdbeb16a
Matt Puchlerz <matt@puchlerz.com>
2009-06-16 14:28:25 -0700

    Got everything set up for some specs.

---
+++ public/dodgebomb/specs/player_spec.js
@@ -0,0 +1,11 @@
+describe('DodgeBomb.Player', {
+  
+  'should be true': function() {
+    value_of(true).should_be(true);
+  },
+  
+  'should be false': function() {
+    value_of(false).should_be(false);
+  }
+  
+});
